e37bc55cc14fd8042ce2c682434e455f56d6c397
github-action-benchmark <github@users.noreply.github.com>
2021-03-25 21:13:41 +0000

    add My Project Go Benchmark (go) benchmark result for f84b6af583b7a747b4340309e499e11ab877825a

---
+++ dev/bench/data.js
@@ -0,0 +1,36 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1616706821083,
+  "repoUrl": "https://github.com/Exitialis/performance-workshop",
+  "entries": {
+    "My Project Go Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "ipneschadin@avito.ru",
+            "name": "Ivan Neschadin"
+          },
+          "committer": {
+            "email": "ipneschadin@avito.ru",
+            "name": "Ivan Neschadin"
+          },
+          "distinct": true,
+          "id": "f84b6af583b7a747b4340309e499e11ab877825a",
+          "message": "fix",
+          "timestamp": "2021-03-26T00:13:09+03:00",
+          "tree_id": "41c0e6f549d2589d69ac78526b0a4d84228fbe7b",
+          "url": "https://github.com/Exitialis/performance-workshop/commit/f84b6af583b7a747b4340309e499e11ab877825a"
+        },
+        "date": 1616706820223,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFib20",
+            "value": 48797,
+            "unit": "ns/op",
+            "extra": "30000 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
